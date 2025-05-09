cc729a6feacf6ab90e9d066419ec07a56694d1ea
ridhocrl <ridhofathurrohman@student.uir.ac.id>
2025-05-09 19:42:50 +0700

    display dan tambah tugas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <script setup>
+import { ref } from 'vue'
+
+const tugas = ref([])
+const newTugas = ref('')
+
+const tambahTugas = () =>{
+  if (newTugas.value.trim() !== '') {
+    tugas.value.push({
+      id: Date.now(),
+      text: newTugas.value,
+      completed: false
+    })
+    newTugas.value = ''
+  }
+}
 </script>
 
 <template>
-  <
+  <input type="text" v-model="newTugas" @keyup.enter="tambahTugas">
+  <button @click="tambahTugas">Tambahkan</button>
+
+  <ul>
+    <li v-for="tugas in tugas" :key="tugas.id">
+      {{  tugas.text }}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
